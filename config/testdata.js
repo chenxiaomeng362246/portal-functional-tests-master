b5dda2f02761d7f84e748e7d9a0d654b37aba5c6
342365 <wsc342365@nd.com.cn>
2021-05-17 17:59:52 +0800

    introduced the dotenv module and add the.env file

--- config/testdata.js
+++
@@ -1,9 +0,0 @@
-export default{
-    "User_account":"nilito8555@astarmax.com",
-    "User_password":"126Legenecy",
-    "Panel_name": ["abbasHome","blackburnCobalt","blackburnNickel"],
-    "Panel_Serial_Number":["75W25-K1HL2E0760075","65W25-KCHB2N0120001","65W26-J2KL2N0380172"],
-    "panel-management_url":"https://panel-management.staging.mypromethean.com/",
-    "user-management_url":"https://user-management.staging.mypromethean.com/",
-    "myPromethean_url":"https://portal.staging.mypromethean.com/",
-}
--- tests/page-objects/panel-management/searchpanel.js
+++ tests/page-objects/panel-management/searchpanel.js
@@ -1,5 +1,5 @@
 import EnrolPage from '../../page-objects/panel-management/enrol-page'
-import testdata from '../../../config/testdata'
+
 class tagpanelPage {
 
     get panelnametext(){ return  $$("//span[contains(text(),'abbasHome')]")}
@@ -7,18 +7,19 @@ class tagpanelPage {
 
     searchexistedpanelPage(panelexist){
         browser.pause(15000)
-        let panelname = testdata['Panel_name']?.[0]
-        let Panel_Serial_Number = testdata['Panel_Serial_Number']?.[0]
+        let panelname = process.env.panelname
+        let Panel_Serial_Number = process.env.Panel_Serial_Number
+        browser.debug()
         if (this.panelnametext[0].isDisplayed()){
             console.log("panel is exist, no need add")
         }else{
             tagmanagementPage.dropaddButton.click()
             EnrolPage.TypemanuallyPage()
-            EnrolPage.panelNameBox.setValue(panelname);
-            EnrolPage.serialNumber.setValue(Panel_Serial_Number);
+            EnrolPage.panelNameBox.setValue(panelname)
+            EnrolPage.serialNumber.setValue(Panel_Serial_Number)
             browser.pause(2000)
-            EnrolPage.nextuplodButton.click();
-            browser.pause(2000);
+            EnrolPage.nextuplodButton.click()
+            browser.pause(2000)
             browser.refresh()
             browser.pause(10000)
         }
@@ -34,10 +35,10 @@ class tagpanelPage {
     verifysearchpanelisexist(panelexist){
         browser.pause(10000)
         if (panelexist ==true){
-            const assert = require('assert');
+            const assert = require('assert')
             assert.strictEqual(this.panelnametext.length,2)
         }else{
-            const assert = require('assert');
+            const assert = require('assert')
             assert.strictEqual(this.panelnameinfotext.isDisplayed(),true)
         }
     }
