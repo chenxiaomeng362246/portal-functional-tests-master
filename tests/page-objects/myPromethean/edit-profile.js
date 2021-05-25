5ea41b1cc77a21c2741fabbd3ea5040ddde7fa86
342365 <wsc342365@nd.com.cn>
2021-05-25 16:44:09 +0800

    Modify edit-profile.js feature failure case

--- tests/page-objects/myPromethean/edit-profile.js
+++ tests/page-objects/myPromethean/edit-profile.js
@@ -2,22 +2,23 @@ import homepage from '../../page-objects/myPromethean/home-page';
 import Commonmethods from '../../utils/commonmethods';
 import { assert } from 'console'
 class editprofilePage {
-    get editprofileButton() { return $("//li[contains(text(),'Edit Profile')]")}
+    get editprofileButton() { return $("//li[contains(text(),'Edit profile')]")}
     get fristnameInput() { return $("input[name='firstName']") }
-    get lnztextimtext() { return $("input[value='lnz_testim_organization']") }
-    get Americacountrytext() { return $("input[value='United States of America']") }
+    get ITAdministratortext() { return $("input[value='IT Administrator']") }
+    get UnitedKingdomcountrytext() { return $("input[value='United Kingdom']") }
     get Englishlanguagetext() { return $("input[value='English (UK)']") }
     get saveeditprofileButton() { return $$("button.portal-button--primary")[2] }
     get editprofileText() { return $("//span[contains(text(),'Profile has been successfully changed.')]")}
-    get countrydropbutton() { return $$("span[class='icon-round-expand_more-24px']")[2]}
+    get countrydropbutton() { return $$("span[class='icon-round-expand_more-24px']")[1]}
     get AustraliacountryInput() { return $("//span[contains(text(),'Australia')]")}
-    get AmericacountryInput() { return $("//span[contains(text(),'America')]")}
-    get languagedropbutton() { return $$("span[class='icon-round-expand_more-24px']")[3]}
+    get UnitedKingdomcountrytextInput() { return $("//span[contains(text(),'United Kingdom')]")}
+    get languagedropbutton() { return $$("span[class='icon-round-expand_more-24px']")[2]}
     get UKlanguagecacountryInput() { return $("//span[contains(text(),'English (UK)')]")}
     get USlanguagecacountryInput() { return $("//span[contains(text(),'English (US)')]")}
-    get organisationbutton() { return $$("span[class='icon-round-expand_more-24px']")[4]}
-    get lnztestimorganisationInput() { return $("//span[contains(text(),'lnz_testim_organization')]")}
-    get lnztestimInput() { return $("//span[contains(text(),'LNZ_Testim')]")}
+    get organisationbutton() { return $$("span[class='icon-round-expand_more-24px']")[0]}
+    get ITAdministratororganisationInput() { return $("//span[contains(text(),'IT Administrator')]")}
+    get TeacherInput() { return $("//span[contains(text(),'Teacher')]")}
+    get editUKlanguageprofileButton() { return $("//li[contains(text(),'Edit Profile')]")}
 
     entereditprofilepage(){
         browser.pause(15000)
@@ -33,14 +34,14 @@ class editprofilePage {
             browser.pause(2000)
             this.fristnameInput.setValue(Commonmethods.randomString())
         }else if(option =="country"){
-            if(this.Americacountrytext.isDisplayed()){
+            if(this.UnitedKingdomcountrytext.isDisplayed()){
                 this.countrydropbutton.click()
                 browser.pause(2000)
                 this.AustraliacountryInput.click()
             }else{
                 this.countrydropbutton.click()
                 browser.pause(2000)
-                this.AmericacountryInput.click()
+                this.UnitedKingdomcountrytextInput.click()
             }
         }else if(option =="language"){
             if(this.Englishlanguagetext.isDisplayed()){
@@ -51,25 +52,39 @@ class editprofilePage {
                 this.languagedropbutton.click()
                 browser.pause(2000)
                 this.UKlanguagecacountryInput.click()
+                this.changethelanguagetoUS()
             }
         }else if(option =="organization"){
-            if(this.lnztextimtext.isExisting()){
+            if(this.ITAdministratortext.isExisting()){
                 this.organisationbutton.click()
                 browser.pause(2000)
-                this.lnztestimInput.click()
+                this.TeacherInput.click()
             }else{            
                 this.organisationbutton.click()
                 browser.pause(2000)
-                this.lnztestimorganisationInput.click()
+                this.ITAdministratororganisationInput.click()
             }
         }
     }
     
     verifynewpasswordpage(){
-        browser.pause(2000)
-        this.saveeditprofileButton.click()
         browser.pause(5000)
-        assert(this.editprofileText.isDisplayed(), true);
+        this.saveeditprofileButton.click()
+        browser.pause(9000)
+        const assert = require('assert')
+        assert.strictEqual(this.editprofileText.isDisplayed(),true)
+    }
+
+    changethelanguagetoUS(){
+        this.verifynewpasswordpage()
+        browser.refresh()
+        browser.pause(12000)
+        homepage.dropdownIcon.click()
+        browser.pause(5000)
+        this.editUKlanguageprofileButton.click()
+        this.languagedropbutton.click()
+        browser.pause(2000)
+        this.USlanguagecacountryInput.click()
     }
 }
 export default new editprofilePage()
